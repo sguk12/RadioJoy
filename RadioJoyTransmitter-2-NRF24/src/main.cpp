74859cc3be7f7cf801b09022202c1ae35f468c13
sguk12 <sguk12@yahoo.com>
2025-09-28 18:12:49 +0800

    Moved the joystick piece to Platformio and removed the throttle bits out of it.

--- RadioJoyTransmitter-2-NRF24/src/main.cpp
+++ RadioJoyTransmitter-2-NRF24/src/main.cpp
@@ -6,15 +6,44 @@
  * 
  */
 
+#include <Arduino.h>
 #include <RF24.h>
 #include "RadioJoy.h"
 
+// #define DEBUG
+
+#ifdef DEBUG
+  #define DEBUG_BEGIN(x) Serial.begin(x)
+  #define DEBUG_PRINTLN(x)  Serial.println(x)
+  #define DEBUG_PRINT(x)  Serial.print(x)
+#else
+  #define DEBUG_BEGIN(x)
+  #define DEBUG_PRINTLN(x)
+  #define DEBUG_PRINT(x)
+#endif
+
+void checkButton(int pin, int button);
+int16_t scanButtons();
+
 RF24 radio(7, 8); // CE and CS pins used for NRF24L01 SPI connection
+
+int16_t previousA0 = 0;
+int16_t previousA1 = 0;
+
+struct Button {
+  unsigned long startDebounce = 0;
+  int state = HIGH;
+};
+
+#define NUMBER_BUTTONS 4
+Button buttons[NUMBER_BUTTONS];
+const int DEBOUNCE = 10;
 
 void setup()
 {
   
-//  Serial.begin(9600);   // Debugging only
+  DEBUG_BEGIN(115200);
+
   radio.begin();
   // Set the PA Level low to prevent power supply related issues since this is a
   // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
@@ -26,18 +55,49 @@ void setup()
   // Start the radio listening for data
   radio.startListening();
 
-  pinMode(5,INPUT_PULLUP);
-  pinMode(6,INPUT_PULLUP);
-  pinMode(A4,INPUT_PULLUP);
-  pinMode(A5,INPUT_PULLUP);
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  pinMode(A2,INPUT_PULLUP);
+  pinMode(A3,INPUT_PULLUP);
 }
 
 void loop()
 {
+  RadioJoystick joystick;
+  joystick.fromToByte = fromJoystickToReceiver;
+  int16_t a0 = analogRead(A0);
+  joystick.axisY = (a0 + previousA0) >> 1;
+  previousA0 = a0;
+  int16_t a1 = analogRead(A1);
+  joystick.axisX = (a1 + previousA1) >> 1;
+  previousA1 = a1;
+  joystick.buttons = scanButtons();
+
+  // Let's make the magnetic field sensor readings somewhat linear
+  // Xcentre=606, k1=0.84=511/606, k2=1.23=511/(1023-606)
+  int Xcentre=592; float kx1=0.86/* =511/592 */, kx2=1.19;/* =511/(1023-592) */
+  if(joystick.axisX < Xcentre){
+    joystick.axisX = (int)(joystick.axisX * kx1);
+  }else{
+    joystick.axisX = (int)(511 + (joystick.axisX - Xcentre) * kx2 );
+  }
+  int Ycentre=433; float ky1=1.18/* =511/433 */, ky2=0.87;/* =511/(1023-433) */
+  if(joystick.axisY < Ycentre){
+    joystick.axisY = (int)(joystick.axisY * ky1);
+  }else{
+    joystick.axisY = (int)(511 + (joystick.axisY - Ycentre) * ky2 );
+  }
+  DEBUG_PRINT("X=");
+  DEBUG_PRINT(joystick.axisX); 
+  DEBUG_PRINT(", Y=");
+  DEBUG_PRINT(joystick.axisY);
+  DEBUG_PRINT(", \tButtons=");
+  DEBUG_PRINT(buttons[0].state);
+  DEBUG_PRINT(buttons[1].state);
+  DEBUG_PRINT(buttons[2].state);
+  DEBUG_PRINTLN(buttons[3].state);
+  
+
   // let's wait for the server's invitation so sen our data
   radio.startListening();                                    // Now, continue listening
   unsigned long started_waiting_at = millis();               // Set up a timeout period, get the current microseconds
@@ -51,115 +111,40 @@ void loop()
   }
       
   if ( timeout ){                                             // Describe the results
-    //Serial.println(F("Failed, response timed out."));
+    DEBUG_PRINTLN(F("Failed, response timed out."));
   }else{
     uint8_t request = 0;
     radio.read( &request, sizeof(uint8_t) );
-//Serial.print("got request: ");
-//Serial.println(request[0]);
     if (fromThrottleToReceiver == request) {
       // if the request was for the throttle data
       // read the data from the sensors
-      RadioJoystick joystick;
-      joystick.fromToByte = fromThrottleToReceiver;
-      joystick.axisX = analogRead(A7);
-      joystick.axisY = analogRead(A6);
-      joystick.axisThrottle = analogRead(A2);
-      joystick.axisPropellor = analogRead(A0);
-      joystick.axisTrim = analogRead(A1);
-      joystick.buttons = scanButtons();
-//Serial.print("X=");
-//Serial.print((int)joystick.axisX);
-//Serial.print(", y=");
-//Serial.println(joystick.axisY); 
-//Serial.print(", Throttle=");
-//Serial.print(joystick.axisThrottle); 
-//Serial.print(", Propellor=");
-//Serial.print(joystick.axisPropellor); 
-//Serial.print(", Trim=");
-//Serial.println(joystick.axisTrim); 
-
-      // Let's make the magnetic field sensor readings somewhat linear
-      // Xcentre=606, k1=0.84=511/606, k2=1.23=511/(1023-606)
-      int Xcentre=592; float kx1=0.86/* =511/592 */, kx2=1.19;/* =511/(1023-592) */
-      if(joystick.axisX < Xcentre){
-        joystick.axisX = (int)(joystick.axisX * kx1);
-      }else{
-        joystick.axisX = (int)(511 + (joystick.axisX - Xcentre) * kx2 );
-      }
-      int Ycentre=433; float ky1=1.18/* =511/433 */, ky2=0.87;/* =511/(1023-433) */
-      if(joystick.axisY < Ycentre){
-        joystick.axisY = (int)(joystick.axisY * ky1);
-      }else{
-        joystick.axisY = (int)(511 + (joystick.axisY - Ycentre) * ky2 );
-      }
-//Serial.print("X out =");
-//Serial.println((int)joystick.axisX);
       delay(2); //this delay is to allow the receiver to prepare for this transmission
 
       radio.stopListening();                                    // First, stop listening so we can talk.
       if (!radio.write( &joystick, sizeof(joystick) )){ // This will block until complete
-        //Serial.println(F("failed"));
+        DEBUG_PRINTLN(F("failed"));
       }
-    }else{
-//        Serial.println("request is not recognised");
     }
   }
 }
 
-struct Button {
-  unsigned long startDebounce = 0;
-  int state = HIGH;
-};
-
-Button buttons[16];
-const int DEBOUNCE = 10;
-
 int16_t scanButtons()
 {
-  // scan the first row
-  digitalWrite(2, LOW);
-  delay(1);
-  checkButton(A4, 0);
-  checkButton(A5, 1);
-  checkButton(5, 2);
-  checkButton(6, 3);
-  digitalWrite(2, HIGH);
-  
-  // scan the second row
-  digitalWrite(3, LOW);
-  delay(1);
-  checkButton(A4, 4);
-  checkButton(A5, 5);
-  checkButton(5, 6);
-  checkButton(6, 7);
-  digitalWrite(3, HIGH);
-
-  // scan the third row
-  digitalWrite(4, LOW);
-  delay(1);
-  checkButton(A4, 8);
-  checkButton(A5, 9);
-  checkButton(5, 10);
-  checkButton(6, 11);
-  digitalWrite(4, HIGH);
+  checkButton(3, 0);
+  checkButton(A2, 1);
+  checkButton(A3, 2);
+  checkButton(4, 3);
 
   int16_t result = 0;
   int16_t currentBit;
   for(int i = 15; i >= 0; i--){
-    if(buttons[i].state == LOW){
+    if(i < NUMBER_BUTTONS && buttons[i].state == LOW){
       currentBit = 1;
     }else{
       currentBit = 0;
     }
     result = result | currentBit << i;
-//Serial.print("button[");
-//Serial.print(i);
-//Serial.print("]=");
-//Serial.print(buttons[i].state);
-//Serial.print("  ");
   }
-//  printBinaryUnsignedInt(result);
   return result;
 }
 
@@ -187,14 +172,4 @@ void checkButton(int pin, int button){
     buttons[button].state = currentState;
   }
 }
-
-//void printBinaryUnsignedInt(unsigned int value)
-//{
-//    for (unsigned int mask = 0x8000; mask; mask >>= 1)
-//    {
-//        Serial.print((mask & value) ? '1' : '0');
-//    }
-//    Serial.println();
-//}
-
 
